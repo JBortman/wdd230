f1ba625f55e3c3033109d88b90b770d0c2af42a6
JBortman <jbortman33@gmail.com>
2023-01-28 03:18:36 -0800

    Home page almost built

--- chamber/scripts/chamber-script.js
+++ chamber/scripts/chamber-script.js
@@ -1,5 +1,24 @@
-const nav = document.querySelector('.navigation');
-const hamButton = document.querySelector('#menu');
+/*-----Hamburger button-----*/
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+}
 
-hamButton.addEventListener('click', ()=>
-{nav.classList.toggle('responsive')}, false);
+const x = document.getElementById("hamBut")
+x.onclick = toggleMenu;
+
+/*-----Day and date for heading-----*/
+const headDate = document.querySelector("#headDate")
+const now = new Date();
+const fullDate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+headDate.innerHTML = `<em>${fullDate}</em>`;
+
+/*-----Date and time for last updated-----*/
+const footYear = document.querySelector('#footYear');
+const footDate = document.querySelector('#footDate');
+
+footYear.textContent = new Date().getFullYear();
+
+const date = new Date().toLocaleDateString();
+const time = new Date().toLocaleTimeString();
+footDate.innerHTML = `Last Updated: ${date} ${time}`;
+
