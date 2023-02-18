af2b06c9e9290665735d7a1e7b3b7a4cb7aadf19
JBortman <jbortman33@gmail.com>
2023-02-18 01:56:03 -0800

    W06 fixes

--- chamber/scripts/dates-between.js
+++ chamber/scripts/dates-between.js
@@ -6,7 +6,7 @@ let text = document.querySelector('#timeVisited')
 if(prevVisit < 1){
     text.textContent = "Welcome! It appears this is your first visit!"
 } else {
-    let totalTime = Math.round((currentVisit - prevVisit)/86400000)
-    text.textContent = "It has been "+totalTime+" days since your last visit"
+    let totalTime = Math.round((currentVisit - prevVisit) / 86400000)
+    text.textContent = "It has been "+ totalTime +" days since your last visit"
 }
 Number(window.localStorage.setItem("previous-visit", currentVisit))
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -4,8 +4,8 @@ const windHTML = document.getElementById('wind').innerText
 temp = parseInt(tempHTML)
 wind = parseInt(windHTML)
 
-if(temp<=50 && wind>3){
-    chill = Math.round(35.74+(0.6215*temp) - (35.75*(wind**0.16)) + (0.4275*temp*(wind**0.16)));
+if(temp <= 50 && wind > 3){
+    chill = Math.round(35.74 + ( 0.6215 * temp) - (35.75 * (wind ** 0.16)) + (0.4275 * temp * (wind ** 0.16)));
     document.querySelector('#chill').innerHTML = chill;
 }
 else
