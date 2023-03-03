ec921d4797bc9d397daa8400c6679f2e62e5dcae
JBortman <jbortman33@gmail.com>
2023-03-03 02:57:35 -0800

    directory started

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,50 @@
+const file = 'json/data.json'
+
+async function getFileData() {
+    const response = await fetch(file);
+        const data = await response.json();
+        //console.table(data.companies)
+        displayInfo(data.companies);
+    };
+
+getFileData();
+
+const displayInfo = (companies) => {
+    const cards = document.querySelector('div.cards')
+
+    companies.forEach((company) => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p1 = document.createElement('p');
+        let p2 = document.createElement('p');
+        let p3 = document.createElement('p');
+        let logo = document.createElement('img');
+        let h3 = document.createElement('h3');
+
+        h2.textContent = company.name;
+        p1.textContent = company.address;
+        p2.textContent = company.phone;
+        p3.textContent = company.website;
+        h3.textContent = `Member Level: ${company.level}`;
+
+        logo.setAttribute('alt', `${company.name} logo`);
+        logo.setAttribute('src', 'images/sweetroll-placeholder.webp');
+        logo.setAttribute('loading', 'lazy');
+
+        
+
+        card.appendChild(h2);
+        card.appendChild(p1);
+        card.appendChild(p2);
+        card.appendChild(p3);
+        card.appendChild(logo);
+        card.appendChild(h3);
+
+        cards.appendChild(card);
+    
+    })
+
+}
+
+
+
